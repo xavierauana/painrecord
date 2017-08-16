48f196a2988d037142787c339741c90e48c48ee7
Xavier Au <xavier.au@anacreation.com>
2017-08-16 11:05:35 +0800

    first commit

---
+++ app/Record.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Record extends Model
+{
+    protected $fillable = [
+        'type',
+        'longitude',
+        'latitude'
+    ];
+}
