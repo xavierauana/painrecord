ff084381e4b86b8db494b336800c54a75ebe7643
Xavier Au <xavier.au@anacreation.com>
2017-08-17 19:11:39 +0800

    update cors 2

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 
 class CORS
 {
@@ -14,9 +15,26 @@ class CORS
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
-                              ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-                              ->header('Access-Control-Allow-Headers',
-                                  'Content-Type, Accept, Authorization, X-Requested-With');
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+
+        if($request->getMethod() == "OPTIONS") {
+
+            $response = new Response();
+            foreach($headers as $key => $value)
+                $response->header->set($key, $value);
+
+            return $response;
+        }
+
+        $response = $next($request);
+
+        foreach($headers as $key => $value)
+            $response->headers->set($key, $value);
+
+        return $response;
     }
 }
